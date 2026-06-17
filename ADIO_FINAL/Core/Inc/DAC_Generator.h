38735b18d7e18299193d3a1bc366625960b3fcc3
Aditya Singh <adityasingh763606@gmail.com>
2026-06-17 18:04:55 +0530

    FINAL CODE FOR ADIO BOARD

---
+++ ADIO_FINAL/Core/Inc/DAC_Generator.h
@@ -0,0 +1,24 @@
+/*
+ * sine_generator.h
+ *
+ *  Created on: Sep 17, 2025
+ *      Author: AdityaSingh
+ */
+
+#ifndef INC_DAC_GENERATOR_H_
+#define INC_DAC_GENERATOR_H_
+
+
+
+#include "stm32f4xx_hal.h"
+/**
+ * @brief Starts the sine wave generation.
+ * @note This function initializes the sine look-up table and starts the
+ *       DAC, DMA, and Timer peripherals.
+ * @param hdac Pointer to the DAC handle.
+ * @param htim Pointer to the Timer handle that provides the trigger.
+ * @retval None
+ */
+void DACGenerator_Start(DAC_HandleTypeDef *hdac, uint32_t channel, float voltage, float vref );
+void DACGenerator_Stop(DAC_HandleTypeDef* hdac, uint32_t channel);
+#endif /* INC_DAC_GENERATOR_H_ */
